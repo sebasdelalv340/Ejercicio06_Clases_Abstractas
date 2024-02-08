e98798dbe42b3507486329d99c1f9acc267ddf72
sebasdelalv340 <sdelgado@iesrafaelalberti.es>
2024-02-08 13:28:46 +0100

    Añadida documentación

--- Ejercicio06_Clases_Abstractas/src/main/kotlin/Empleados.kt
+++ Ejercicio06_Clases_Abstractas/src/main/kotlin/Empleados.kt
@@ -1,32 +1,85 @@
 
-
+/**
+ * Clase abstracta que representa a un empleado.
+ * @property nombre El nombre del empleado.
+ * @property id El identificador único del empleado.
+ */
 abstract class Empleado(val nombre: String, val id: Int) {
+
+    /**
+     * Calcula el salario del empleado.
+     * @return El salario del empleado.
+     */
     abstract fun calcularSalario(): Double
 }
 
+
+/**
+ * Clase que representa a un empleado que recibe un salario por hora trabajada.
+ * @param nombre El nombre del empleado.
+ * @param id El identificador único del empleado.
+ * @param horasTrabajadas Las horas trabajadas por el empleado.
+ * @param tarifaPorHoras La tarifa por horas del empleado.
+ */
 class EmpleadoPorHora(nombre: String, id: Int, val horasTrabajadas: Double, val tarifaPorHoras: Double): Empleado(nombre, id) {
 
+    /**
+     * Constructor secundario para permitir la entrada de horas trabajadas como Int.
+     */
     constructor(nombre: String, id: Int, horasTrabajadas: Int, tarifaPorHoras: Double): this(nombre, id, horasTrabajadas.toDouble(), tarifaPorHoras)
 
+    /**
+     * Calcula el salario de EmpleadoPorHora.
+     * @return El salario sobreescrito de EmpleadoPorHora.
+     */
     override fun calcularSalario(): Double {
         return horasTrabajadas * tarifaPorHoras
     }
 }
 
+
+/**
+ * Clase que representa a un empleado con un salario fijo.
+ * @param nombre El nombre del empleado.
+ * @param id El identificador único del empleado.
+ * @param salarioFijo El salario fijo del empleado.
+ * @param numPagas El número de pagas en las que se divide el salario fijo.
+ */
 class EmpleadoFijo(nombre: String, id: Int, val salarioFijo: Double, val numPagas: Int): Empleado(nombre, id) {
 
+    /**
+     * Constructor secundario para permitir la entrada de salario fijo como Int.
+     */
     constructor(nombre: String, id: Int, salarioFijo: Int, numPagas: Int): this(nombre, id, salarioFijo.toDouble(), numPagas)
 
+    /**
+     * Calcula el salario de EmpleadoFijo
+     * @return El salario sobreescrito de EmpleadoFijo
+     */
     override fun calcularSalario(): Double {
         return salarioFijo / numPagas
     }
 }
 
+
+/**
+ * Clase que representa un departamento que contiene una lista de empleados.
+ * @property empleados La lista de empleados del departamento.
+ */
 class Departamento(val empleados: MutableList<Empleado>) {
+
+    /**
+     * Agrega un empleado a la lista de empleados del departamento.
+     * @param empleado El empleado a agregar.
+     */
     fun agregarEmpleado(empleado: Empleado) {
         empleados.add(empleado)
     }
 
+    /**
+     * Calcula el salario total de todos los empleados en el departamento.
+     * @return El salario total del departamento.
+     */
     fun calculaSalarioTotal() {
         empleados.sumOf { it.calcularSalario()}
     }
--- Ejercicio06_Clases_Abstractas/src/main/kotlin/Figura.kt
+++ Ejercicio06_Clases_Abstractas/src/main/kotlin/Figura.kt
@@ -1,35 +1,100 @@
 import kotlin.math.PI
 
+/**
+ * Clase abstracta que representa una figura geométrica.
+ * @property color El color de la figura.
+ */
 abstract class Figura(val color: String) {
+
+    /**
+     * Calcula el área de la figura.
+     * @return El área de la figura.
+     */
     abstract fun area(): Double
+
+    /**
+     * Calcula el perímetro de la figura.
+     * @return El perímetro de la figura.
+     */
     abstract fun perimetro(): Double
 }
 
+
+/**
+ * Clase que representa un círculo.
+ * @param color El color del círculo.
+ * @param radio El radio del círculo.
+ */
 class Circulo(color: String, val radio: Double): Figura(color) {
+
+    /**
+     * Calcula el área sobreescrita de Círculo.
+     * @return El área sobreescrita de Círculo.
+     */
     override fun area(): Double {
         return PI * (radio * radio)
     }
 
+    /**
+     * Calcula el perímetro sobreescrito de Círculo.
+     * @return El perímetro sobreescrito de Círculo.
+     */
     override fun perimetro(): Double {
         return 2 * PI * radio
     }
 }
 
+
+/**
+ * Clase abstracta que representa un rectángulo.
+ * @param color El color del rectángulo.
+ * @param base La base del rectángulo.
+ * @param altura La altura del rectángulo.
+ */
 abstract class Rectangulo(color: String, val base: Double, val altura: Double): Figura(color) {
+
+    /**
+     * Calcula el área sobreescrita de Rectángulo.
+     * @return El área sobreescrita de Rectángulo.
+     */
     override fun area(): Double {
         return base * altura
     }
 
+    /**
+     * Calcula el perímetro sobreescrito de Rectángulo.
+     * @return El perímetro sobreescrito de Rectángulo.
+     */
     override fun perimetro(): Double {
         return (base + altura) * 2
     }
 }
 
-class Triangulo(color: String, base: Double, altura: Double, val lado1: Double, val lado2: Double, val lado3: Double): Rectangulo(color, base, altura) {
+
+/**
+ * Clase que representa un triángulo.
+ * @param color El color del triángulo.
+ * @param base La base del triángulo.
+ * @param altura La altura del triángulo.
+ * @param lado1 La longitud del primer lado del triángulo.
+ * @param lado2 La longitud del segundo lado del triángulo.
+ * @param lado3 La longitud del tercer lado del triángulo.
+ */
+class Triangulo(color: String, base: Double, altura: Double, val lado1: Double, val lado2: Double, val lado3: Double): Rectangulo(color, base, altura) { // Hereda de Rectángulo porque tienen las propiedades en común.
+
+    /**
+     * Calcula el área sobreescrita de Triángulo.
+     * @return El área sobreescrita de Triángulo.
+     */
     override fun area(): Double {
         return (base * altura) / 2
     }
 
+
+    /**
+     * Calcula el perímetro sobreescrito de Triángulo.
+     * @return El perímetro sobreescrito de Triángulo.
+     */
     override fun perimetro(): Double {
         return lado1 + lado2 + lado3
     }
